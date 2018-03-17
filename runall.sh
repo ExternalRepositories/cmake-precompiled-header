ec7d8551968554a19d0066e01fd68a8396ea5dd4
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2018-03-17 08:54:13 +0100

    Improving travis build
    * Specifying explicit compiler versions (C++14 test fails with default gcc)
    * Added clang versions to test
    * Generalized the linux test script to support more generators
    * Added sleeps between the tests in runall, to fix an issue when testing
      the ninja generator. The generator is based on the modification time,
      and wouldn't detect changes without some delay.
    * Added tests with the ninja generator

--- runall.sh
+++ runall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -exu
 set -e
 
+: "${generator:=Unix Makefiles}"
+
 buildroot=$(dirname $(readlink -f $0))/build
-generator="Unix Makefiles"
 mkdir -p "$buildroot"
 for test in $(ls test); do
 
@@ -22,11 +23,13 @@ for test in $(ls test); do
    cmake --build .
    EXPECTED_PCH=1 ctest
    # Modify PCH, compile, test (dependencies should work)
+   sleep 1
    echo "#undef PCH" >> "$source/test-pch.h"
    echo "#define PCH 2" >> "$source/test-pch.h"
    cmake --build .
    EXPECTED_PCH=2 ctest
    # Restore PCH with old timestamp (already compiled PCH should be used)
+   sleep 1
    cp -a "$source/test-pch-old.h" "$source/test-pch.h"
    [ -f "$source/test.c" ] && touch "$source/test.c"
    [ -f "$source/test.cpp" ] && touch "$source/test.cpp"
